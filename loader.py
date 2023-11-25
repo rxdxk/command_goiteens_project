ded075769bd36b91393058684263585909173d72
rxdxk <rodionovkirill330@gmail.com>
2023-11-25 13:10:00 +0200

    updated

--- loader.py
+++ loader.py
@@ -2,6 +2,8 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
+import config
+
 bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import aiogram
+import logging
+import asyncio
+import sys
+
+from loader import dp, bot
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
