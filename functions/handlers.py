4cca39ec6c5702cc39ca8befb7e455633bed072e
Your Name <you@example.com>
2023-11-28 18:11:12 +0200

    new hahahnges

--- functions/handlers.py
+++ functions/handlers.py
@@ -2,10 +2,10 @@ from aiogram import types,Router,F
 from aiogram.filters.command import Command, CommandObject
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
-
+from tests import *
 from .keyboards import *
 from db_api.db_users import *
-from loader import dp, R, db_users 
+from loader import dp, R, db_users
 
 R = Router()
 
@@ -16,6 +16,66 @@ class Form(StatesGroup):
     surname = State()
     phone = State()
 
+
+
+
+class ST(StatesGroup):
+    q1 = State()
+    q2 = State()
+    q3 = State()
+    q4 = State()
+    q5 = State()
+    q6 = State()
+
+correct_answers = 0
+lvlok = " "
+
+def ques(current_ques):
+    a = questions[str(current_ques)]
+    txt = a["question"]
+    return txt
+def gen_keyb(num):
+    but = []
+    a = questions[str(num)] #1
+    b = a["options"] # "0": "goed",etc
+    for x in range(len(b)):
+        txt = b[f"{x}"]
+        but.append([types.KeyboardButton(text = f"{txt}")])
+        keyb  = types.ReplyKeyboardMarkup(keyboard=but) 
+    return keyb
+
+def check_ans(current_ques,user_ans):
+    global correct_answers
+    a = questions[str(current_ques)] #1
+    answer = a["answer"]
+    print(answer)
+    if user_ans == answer:
+        correct_answers += 1
+        return True
+    else:
+        return False
+
+def lvl(correct_answers):
+    if correct_answers == 1:
+        lvl = "A1"
+        return lvl
+    elif correct_answers == 2:
+        lvl  = "A2"
+        return lvl
+    elif correct_answers == 3:
+        lvl = "B1"
+        return lvl
+    elif correct_answers ==4:
+        lvl = "B2"
+        return lvl
+    elif correct_answers == 5:
+        lvl = "C1"
+        return lvl
+    elif correct_answers == 6:
+        lvl =" C2"
+        return lvl
+
+
 @R.message(Command("info"))
 async def start_func(message: types.Message, state: FSMContext):
     await message.answer('''<b>Що може цей бот?</b>
@@ -25,6 +85,7 @@ async def start_func(message: types.Message, state: FSMContext):
     -Давати завдання
     -Допомогти тобі провести час із користю</i>
                             ''')
+
 
 @dp.message(F.text == "Реєстрація")
 async def reg_func(message: types.Message):
@@ -41,7 +102,7 @@ async def name_func(message: types.Message, state: FSMContext):
     
     name = message.text
 
-    if len(name) < 12 and name.isalpha():
+    if len(name) < 12  and len(name) >= 3 and name.isalpha():
         await message.answer(f"Hello {name}")
         await state.update_data(name=name)
         await state.set_state(Form.surname)
@@ -52,11 +113,10 @@ async def name_func(message: types.Message, state: FSMContext):
         await message.answer("Введіть коректне ім'я")
 
 @R.message(Form.surname)
-
 async def surname_func(message: types.Message, state: FSMContext):
     surname = message.text
 
-    if len(surname) < 20 and surname.isalpha():
+    if len(surname) < 20  and surname.isalpha() :
         await state.update_data(surname=surname)
         await state.set_state(Form.phone)
         await message.answer(f"Тепер введи свій номер телефону ,починаючи з +38")
@@ -68,8 +128,9 @@ async def surname_func(message: types.Message, state: FSMContext):
 @R.message(Form.phone)
 async def phone_func(message: types.Message, state: FSMContext):
     phone = message.text
-
-    if len(phone) == 10 and phone.isnumeric():
+    global cur_ques
+    genius_num = ["1234567890","0987654321"]
+    if len(phone) == 10 and phone.isnumeric() and phone not in genius_num:
         await state.update_data(phone=phone)
         data  = await state.get_data()
         
@@ -84,10 +145,186 @@ async def phone_func(message: types.Message, state: FSMContext):
 
         await message.answer("Реєстрацію завершено!")
         await message.answer("Ти вже почав свій рух і скоро ти будеш говорити англіською як він:")
-        await message.answer("А зараз ми проунуємо вам пройти короткий тест на ваш рівень англійської мови")
-
+        await message.answer("А зараз ми пропонуємо вам пройти короткий тест на ваш рівень англійської мови")
+        cur_ques = 1
+        keyb = gen_keyb(cur_ques)
+        await state.set_state(ST.q1)
+        await message.answer("Натискайте на відповіді ,які вважайте правильними")
+        txt = ques(cur_ques)
+        await message.answer(txt,reply_markup=keyb)
     else:
         await state.set_state(Form.phone)
         await message.answer("Введіть коректний номер")
+
+@R.message(ST.q1)
+async def question1(message: types.Message, state: FSMContext):
+    global cur_ques
+    user_ans = message.text
+    answer = check_ans(cur_ques,user_ans)
+    if answer == True:
+        cur_ques += 1
+        print(cur_ques)
+        keyb = gen_keyb(cur_ques)
+        await message.answer("+")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q2)
+    else:
+        cur_ques += 1
+        keyb = gen_keyb(cur_ques)
+        await message.answer("-")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q2)
+
+@R.message(ST.q2)
+async def question1(message: types.Message, state: FSMContext):
+    global cur_ques
+    print(cur_ques)
+    user_ans = message.text
+    answer = check_ans(cur_ques,user_ans)
+    if answer == True:
+        cur_ques += 1
+        print(cur_ques)
+        keyb = gen_keyb(cur_ques)
+        await message.answer("+")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q3)
+    else:
+        cur_ques += 1
+        keyb = gen_keyb(cur_ques)
+        await message.answer("-")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q3)
+
+@R.message(ST.q3)
+async def question1(message: types.Message, state: FSMContext):
+    global cur_ques
+    print(cur_ques)
+    user_ans = message.text
+    answer = check_ans(cur_ques,user_ans)
+    if answer == True:
+        cur_ques += 1
+        #dddd
+        keyb = gen_keyb(cur_ques)
+        await message.answer("+")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q4)
+    else:
+        cur_ques += 1
+        keyb = gen_keyb(cur_ques)
+        await message.answer("-")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q4)
+
+@R.message(ST.q2)
+async def question1(message: types.Message, state: FSMContext):
+    global cur_ques
+    print(cur_ques)
+    user_ans = message.text
+    answer = check_ans(cur_ques,user_ans)
+    if answer == True:
+        cur_ques += 1
+        keyb = gen_keyb(cur_ques)
+        await message.answer("+")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q3)
+    else:
+        cur_ques += 1
+        keyb = gen_keyb(cur_ques)
+        await message.answer("-")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q3)
+    
+@R.message(ST.q3)
+async def question1(message: types.Message, state: FSMContext):
+    global cur_ques
+    print(cur_ques)
+    user_ans = message.text
+    answer = check_ans(cur_ques,user_ans)
+    if answer == True:
+        cur_ques += 1
+        keyb = gen_keyb(cur_ques)
+        await message.answer("+")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q4)
+    else:
+        cur_ques += 1
+        keyb = gen_keyb(cur_ques)
+        await message.answer("-")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q4)
+
+@R.message(ST.q4)
+async def question1(message: types.Message, state: FSMContext):
+    global cur_ques
+    user_ans = message.text
+    answer = check_ans(cur_ques,user_ans)
+    if answer == True:
+        cur_ques += 1
+        keyb = gen_keyb(cur_ques)
+        await message.answer("+")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q5)
+    else:
+        cur_ques += 1
+        keyb = gen_keyb(cur_ques)
+        await message.answer("-")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q5)
+
+@R.message(ST.q5)
+async def question1(message: types.Message, state: FSMContext):
+    global cur_ques
+    user_ans = message.text
+    answer = check_ans(cur_ques,user_ans)
+    if answer == True:
+        cur_ques += 1
+        keyb = gen_keyb(cur_ques)
+        await message.answer("+")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q6)
+    else:
+        cur_ques += 1
+        keyb = gen_keyb(cur_ques)
+        await message.answer("-")
+        txt = ques(cur_ques)
+        await message.answer(f"{txt}",reply_markup=keyb)
+        await state.set_state(ST.q6)
+
+@R.message(ST.q6)
+async def question1(message: types.Message, state: FSMContext):
+    global cur_ques
+    user_ans = message.text
+    answer = check_ans(cur_ques,user_ans)
+    if answer == True:
+        keyb = gen_keyb(cur_ques)
+        await message.answer("+")
+        lvlok = lvl(correct_answers)
+        await message.answer(f"Ти набрав {correct_answers} вірних відповідей")
+        await message.answer(f"Нашим дуже крутим аналізатором було вирішено ,що у тебе {lvlok}")
+    else:
+        keyb = gen_keyb(cur_ques)
+        await message.answer("-")
+        await message.answer(f"Ти набрав {correct_answers} вірних відповідей")
+        await message.answer(f"Нашим дуже крутим аналізатором було вирішено ,що у тебе {lvlok}")
+
+
+    
+    
+    
+    
+
 
 
---
+++ tests.py
@@ -0,0 +1,55 @@
+questions = {
+    "1": {
+        "question": "How do you correctly write the past tense form of the verb 'to go'?",
+        "options": {
+            "0": "goed",
+            "1": "went",
+            "2": "gone"
+        },
+        "answer": "went"
+    },
+    "2": {
+        "question": "Which word is a synonym for 'happy' in English?",
+        "options": {
+            "0": "Sad",
+            "1": "Joyful",
+            "2": "Angry"
+        },
+        "answer": "Joyful"
+    },
+    "3": {
+        "question": "How do you say 'Я хочу їсти' in English?",
+        "options": {
+            "0": "I want to eat",
+            "1": "I wanting eating",
+            "2": "I wanted eat"
+        },
+        "answer": "I want to eat"
+    },
+    "4": {
+        "question": "Which word is the opposite of 'big' in English?",
+        "options": {
+            "0": "Small",
+            "1": "Tall",
+            "2": "Large"
+        },
+        "answer": "Small"
+    },
+    "5": {
+        "question": "How do you correctly write the sentence 'She often plays tennis.' in English?",
+        "options": {
+            "0": "She plays tennis often.",
+            "1": "She often play tennis.",
+            "2": "She playing tennis often."
+        },
+        "answer": "She plays tennis often."
+    },
+    "6": {
+        "question": "What does the tense of a sentence show in English grammar?",
+        "options": {
+            "0": "The gender of the subject",
+            "1": "The time of the action",
+            "2": "The length of the sentence"
+        },
+        "answer": "The time of the action"}
+}
